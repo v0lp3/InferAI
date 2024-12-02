3b20f54d99b7e052f42d9f27f97fa4b56b7429be
v0lp3 <andreamaujeri@gmail.com>
2024-12-02 17:05:19 +0100

    Minor fixes

--- infer-worker/src/definitions.py
+++ infer-worker/src/definitions.py
@@ -6,3 +6,7 @@ with open(rabbitmq_pass_filepath, "r") as f:
     RABBITMQ_PASSWORD = f.read().strip()
 
 RABBITMQ_CREDENTIALS = (environ.get("RABBITMQ_USER", "user"), RABBITMQ_PASSWORD)
+
+STATUS_NO_VULN_FOUND = 201
+STATUS_REPO_NOT_FOUND = 404
+STATUS_OK = 200
